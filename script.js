41dabec3ca5a9af2a673258198a8580b6cc51d44
yasminhb <68723198+yasminhb@users.noreply.github.com>
2020-10-15 22:07:08 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Face Tracking & Emotion Detection Code by Web Dev Simplified  21/05/19
+// ^ Code accessed via https://github.com/WebDevSimplified/Face-Detection-JavaScript
+// Face Tracking/Emotion Detection code remixed by Yasmin Harith Brewer 16/10/20
+// Animations and Images created by Yasmin Harith Brewer
+// Happy sound via https://www.youtube.com/watch?v=IQL53eQ0cNA
+// Sad sound via https://www.youtube.com/watch?v=q76bMs-NwRk
+// Neutral sound via https://www.youtube.com/watch?v=UgHKb_7884o
+// Angry sound via https://www.youtube.com/watch?v=Fz1SAYd7js0
+// Disgusted sound via https://www.youtube.com/watch?v=eKFTSSKCzWA
+// Special Thanks to Karen Ann Donnachie, Andy Simionato and Kenny Yu
+// p5.play resource: https://molleindustria.github.io/p5.play/
+
 var result;
 var happysky, sadsky, angrysky, neutralsky, surprisedsky, disgustedsky, fearsky;
 var rain, lightning, puddle, snow, rainbow;
@@ -10,14 +22,13 @@ var digustedflower1, disgustedflower2, disgustedflower3;
 var happysound, sadsound, angrysound, disgustedsound, neutralsound;
 var mode;
 var landingpage;
-var sound, button, input;
 
 
 const video = document.getElementById('video');
 
 function preload(){
   
-landingpage= loadImage ('landing page.jpg');
+landingpage= loadImage ('landingpage.jpg');
 
   happysky = loadImage('images/happysky.jpg');
     sadsky = loadImage ('images/sadsky.jpg');
@@ -61,68 +72,23 @@ landingpage= loadImage ('landing page.jpg');
     snow = loadAnimation('snow/snow1.png','snow/snow2.png','snow/snow3.png');
     rainbow = loadAnimation('rainbow/rainbow1.png','rainbow/rainbow2.png','rainbow/rainbow3.png','rainbow/rainbow4.png','rainbow/rainbow5.png','rainbow/rainbow6.png','rainbow/rainbow7.png','rainbow/rainbow8.png','rainbow/rainbow9.png','rainbow/rainbow10.png','rainbow/rainbow11.png','rainbow/rainbow12.png','rainbow/rainbow13.png','rainbow/rainbow14.png','rainbow/rainbow15.png','rainbow/rainbow16.png','rainbow/rainbow17.png','rainbow/rainbow18.png','rainbow/rainbow19.png');
 
-    happysound = loadSound ('sounds/happy sound.mp3');
-    sadsound = loadSound ('sounds/sad sound.mp3');
-    neutralsound = loadSound('sounds/neutral sound.mp3');
-    angrysound = loadSound('sounds/angry sound.mp3');
-    disgustedsound = loadSound('sounds/disgusted sound.mp3');
+    happysound = loadSound ('sounds/happysound.mp3');
+    sadsound = loadSound ('sounds/sadsound.mp3');
+    neutralsound = loadSound('sounds/neutralsound.mp3');
+    angrysound = loadSound('sounds/angrysound.mp3');
+    disgustedsound = loadSound('sounds/disgustedsound.mp3');
 
 }
 
     function setup() {
-        createCanvas(windowWidth,windowHeight);
+        createCanvas(900,675);
         mode = 0; 
-        // textSize(21);
-        // createButton('Sound');
-        // button.position(20,40);
-        // button.mousePressed
-
-
     }
 
     function draw(){
       if (mode==0){
-        push();
-        scale(0.3,0.3);
+        scale(0.26,0.26);
         image(landingpage,0,0);
-        pop();
-
-        // scale(windowWidth,windowHeight);
-        
-        
-        // image(image1,0,0);
-        // // ellipse(100,100);
-        // // fill(385);
-        // text('Hi there!',windowWidth/2,windowHeight/3);
-        // text('Welcome the Garden of Pathetic Fallacy!',windowWidth/6,windowHeight/2);
-        // text('To access the garden, make sure you allow this page to access your webcam, also make sure you are using chrome as a browser!',windowWidth/6,windowHeight/4);
-        // text('Your facial expressions will be analysed via your webcam and will help you to interact with the garden', windowWidth/6, windowHeight/5);
-        // text('Try to make a bunch of different facial expressions and have fun!',windowWidth/6,windowHeight/6);
-        // text('Press ENTER to start',windowWidth/6,windowHeight/7);
-
-        // happysky.hide();
-        // neutralsky.hide();
-        
-
-  
-        
-    
-        
-   
-
-        // Hi there!
-
-        // Welcome to the Garden!
-        
-        // To access the garden, make sure you allow this page access to your webcam.
-        
-        // Your facial expressions will be analysed via your webcam and help you to interact with the garden.
-        
-        // Have fun!
-        
-        // Press ENTER to start
-
-
         fill(0);
       }
 
@@ -132,8 +98,6 @@ landingpage= loadImage ('landing page.jpg');
       if (keyCode===ENTER){
         mode=1;
         clear(landingpage);
-        // clear(text);
-        // clear(rect);
       }
     }
 
@@ -164,268 +128,144 @@ video.addEventListener('play', () => {
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    // console.log(resizedDetections);
-    // console.log(detections[0].expressions);
-    var expressions = detections[0].expressions;
-    // var array = [expressions.happy, expressions.sad, expressions.neutral, expressions.angry, expressions.surprised, expressions.digust]
-    
-
-    var values = {1:expressions.happy, 2:expressions.sad, 3:expressions.neutral, 4:expressions.angry, 5:expressions.surprised, 6:expressions.disgusted, 7:expressions.fearful};
-    result = Object
+    if(detections[0]==null){
+      console.log("No Face Detected");
+    } else{
+      var expression = detections[0].expressions;
+      var values = {1:expression.happy, 2:expression.sad, 3:expression.neutral, 4:expression.angry, 5:expression.surprised, 6:expression.disgusted, 7:expression.fearful};
+      result = Object
       .keys(values)
       .sort(function (a,b){ return values [b] - values[a]; })
       .slice(0,10)
       .map(Number)
 
-      console.log(result[0]);
-    
-      // if(result[0] == 1){
-      //   ellipse(random(width), random(height),10,10);
-      // }
-
-      // if(result[0] == 1){
-      //   image(happysky,0,0);
-      // }
-
-     
-
-      // let er = 
-      //   showResults(er)
+      if(result[0] == 1){
+        image(happysky,0,0);
+        happysky.resize(900,675);
+        animation(happyflower1,180,400);
+        animation(happyflower2,450,300);
+        animation(happyflower3,700,400);
+        sadsound.stop();
+        neutralsound.stop();
+        angrysound.stop();
+        disgustedsound.stop();
         
-      // function showResults(results){ 
+      }
 
-      //   if(result[0]!==undefined){
-      //     if(result[0]==1){
-      //       fill(0);
-      //       ellipse(random(width),random(height),10,10);
-            
-      //     }
-
-      // }
-     
+      if(result[0] == 2){
+        image(sadsky,0,0);
+        sadsky.resize(900,675);
+        animation(rain,450,330);
+        animation(puddle,450,330);
+        animation(flower1,180,400);
+        animation(flower2,450,300);
+        animation(flower3,700,400);
+        happysound.stop();
+        neutralsound.stop();
+        angrysound.stop();
+        disgustedsound.stop();
+      }
     
-      // }
-
-
-      // if (result[0] = 1){
-      //   ellipse(100,100);
-      //   fill(0);
-      // }
-
-
-  // if(result[0] = 1){
-  //   console.log("happy")
-  // } else if(result[0] !=1){
-  //   console.log("not happy")
-  // }
-
+      if(result[0] == 3){
+        image(neutralsky,0,0);
+        neutralsky.resize(900,675);
+        animation(snow,450,330);
+        animation(neutralflower1,180,400);
+        animation(neutralflower2,450,300);
+        animation(neutralflower3,700,400);
+        happysound.stop();
+        sadsound.stop();
+        angrysound.stop();
+        disgustedsound.stop();
+      }
+    
+      if(result[0] == 4){
+        image(angrysky,0,0);
+        angrysky.resize(900,675);
+        animation(lightning,450,330);
+        animation(angryflower1,10,410);
+        animation(angryflower2,250,320);
+        animation(angryflower3,480,410);
+        happysound.stop();
+        sadsound.stop();
+        neutralsound.stop();
+        disgustedsound.stop();
+      }
+    
+      if(result[0] == 5){
+        image(surprisedsky,0,0);
+        surprisedsky.resize(900,675);
+        animation(surprisedflower1,180,400);
+        animation(surprisedflower2,450,300);
+        animation(surprisedflower3,700,400);
+        happysound.stop();
+        sadsound.stop();
+        neutralsound.stop();
+        angrysound.stop();
+        disgustedsound.stop();
+      }
+    
+      if(result[0] == 6){
+        image(disgustedsky,0,0);
+        disgustedsky.resize(900,675);
+        animation(disgustedflower1,180,400);
+        animation(disgustedflower2,450,300);
+        animation(disgustedflower3,700,400);
+        happysound.stop();
+        sadsound.stop();
+        neutralsound.stop();
+        angrysound.stop();
+      }
+    
+      if(result[0] == 7){
+        image(fearsky,0,0);
+        fearsky.resize(900,675);
+        animation(rainbow,450,330);
+        animation(fearflower1,180,400);
+        animation(fearflower2,450,300);
+        animation(fearflower3,700,400);
+        happysound.stop();
+        sadsound.stop();
+        neutralsound.stop();
+        angrysound.stop();
+        disgustedsound.stop();
+      }
   
-
-  if(result[0] == 1){
-    image(happysky,0,0);
-    happysky.resize(900,675);
-
-
-    // push(); 
-    // // scale(2,2);
-    // image(happysky,0,0,happysky.width*0.5, happysky.height*0.5);
-    // pop();
-    // var x=happysky.width*0.5*0.25
-    // // happysky.resize(900,675);
-
-    // animation(happyflower1,x,400);
-    // animation(happyflower2,2*x,300);
-    // animation(happyflower3,3*x,400);
-
-    animation(happyflower1,180,400);
-    animation(happyflower2,450,300);
-    animation(happyflower3,700,400);
+    }
     
-    // happyflower1.looping = false;
-    // happyflower2.looping = false;
-    // happyflower3.looping = false;
-  }
-
-  if(result[0] == 2){
-    image(sadsky,0,0);
-    sadsky.resize(900,675);
-    animation(rain,450,330);
-    animation(puddle,450,330);
-    animation(flower1,180,400);
-    animation(flower2,450,300);
-    animation(flower3,700,400);
-  }
-
-  if(result[0] == 3){
-    image(neutralsky,0,0);
-    neutralsky.resize(900,675);
-    animation(snow,450,330);
-    animation(neutralflower1,180,400);
-    animation(neutralflower2,450,300);
-    animation(neutralflower3,700,400);
-  }
-
-  if(result[0] == 4){
-    image(angrysky,0,0);
-    angrysky.resize(900,675);
-    animation(lightning,450,330);
-    animation(angryflower1,10,410);
-    animation(angryflower2,250,320);
-    animation(angryflower3,480,410);
-  }
-
-  if(result[0] == 5){
-    image(surprisedsky,0,0);
-    surprisedsky.resize(900,675);
-    animation(surprisedflower1,180,400);
-    animation(surprisedflower2,450,300);
-    animation(surprisedflower3,700,400);
-  }
-
-  if(result[0] == 6){
-    image(disgustedsky,0,0);
-    disgustedsky.resize(900,675);
-    animation(disgustedflower1,180,400);
-    animation(disgustedflower2,450,300);
-    animation(disgustedflower3,700,400);
-  }
-
-  if(result[0] == 7){
-    image(fearsky,0,0);
-    fearsky.resize(900,675);
-    animation(rainbow,450,330);
-    animation(fearflower1,180,400);
-    animation(fearflower2,450,300);
-    animation(fearflower3,700,400);
-  }
-
-
-
-  // }else if(result[0] !==1){
-  //   happysky.hide;
-  // }
-
-
-
-    // console.log(array);
-    // expressions.happy 
-    // console.log(expressions.happy)
-    // expressions.sad
-    // expressions.neutral
-    // expressions.angry
-    // expressions.surprised
-    // expressions.disgust
-    // expressions.fearful
-
-    // shows the live list of facial expressions on face
-    // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    // faceapi.draw.drawDetections(canvas, resizedDetections)
-    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
 
 function mousePressed(){
 
-// if(result[0]==1){
-// sound = happysound
-
+if(result[0]==1){
+  happysound.play();
+} else {
+happysound.stop();
 }
 
-// sadsound.play();
-  // if(result[0] == 2){
-  //   sadsound.isPlaying();
-  //   }else{
-  //     sadsound.stop();
-  //   }
-  // }
-
-  // var sound;
-
-
-
-  // if(result[0]==1){
-  //   sound = happysound;
-  // } 
-
-  // if(button clicked= true)
-  // sound.play
-
-  if(result[0]==1){
-    happysound.play();
-  } else {
-  happysound.stop();
-  }
-
 if(result[0]==2){
-  sadsound.play();
+sadsound.play();
 } else {
 sadsound.stop();
 }
 
 if(result[0]==3){
-  neutralsound.play();
+neutralsound.play();
 } else {
 neutralsound.stop();
 }
 
 if(result[0]==4){
-  angrysound.play();
+angrysound.play();
 } else {
 angrysound.stop();
 }
 
 if(result[0]==6){
-  disgustedsound.play();
+disgustedsound.play();
 } else {
 disgustedsound.stop();
 }
 
-
-
-
-
-
-
-  // if (sadsound.isPlaying()){ // checks if the song is playing
-  //   sadsound.stop();
-  // }
-
-
-  // if (song.isPlaying()){ // checks if the song is playing
-  //   song.stop(); // if it is, then stop the song
-  //   fill(0,255,0);
-  // } 
-  // else{
-  //   fill(255,0,0); // changes the fill to red
-  //   song.play(); // if it isn't then play the song
-
-// if(result[0] == 1){
-//     happysound.isPlaying();
-//     }else{
-//       happysound.stop();
-//     }
-//   }
-
-
-// }
-
-
-// if(angrysound.isPlaying()){
-//   angrysound.stop();
-// }
-
-// function keyPressed(){
-//   title loading screen will say 'welcome to the garden of _____, garden takes about 10 seconds to load, if you so desire you may click the screen to enable sound, try to be as expressive with your facial features as you possibly can, press any key to enter'
-// }
-
-// }
-
-// function draw() {
-//     background(220);
-    
-//     if(result[0] == 1){
-//           image(happysky,0,0);
-//           happysky.resize(900,675);
-//         }
-//       }
+}
